565406b5d0f067c64788921a4fa7b821499edb32
Justyna Chmielińska <j.em.chmielinska@gmail.com>
2023-04-13 22:55:49 +0200

    proj name

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 GIT_COMMIT=$(git rev-parse --short HEAD)
 
-docker build -t europe-north1-docker.pkg.dev/${TF_VAR_project}/chonky-repository:${GIT_COMMIT} \
+docker build -t europe-north1-docker.pkg.dev/winged-plate-378113/chonky-repository:${GIT_COMMIT} \
     -e BUCKET_ID=${BUCKET_ID} \
     -e DB_FILE=${DB_FILE} \
     .
