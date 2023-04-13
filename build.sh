3153c3e3561d46615067bc45465bc12a8bbf6e09
Justyna Chmielińska <j.em.chmielinska@gmail.com>
2023-04-13 22:58:53 +0200

    docker push

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 GIT_COMMIT=$(git rev-parse --short HEAD)
 
-docker build -t europe-north1-docker.pkg.dev/winged-plate-378113/chonky-repository:${GIT_COMMIT} \
-    -e BUCKET_ID=${BUCKET_ID} \
-    -e DB_FILE=${DB_FILE} \
-    .
+docker build -t europe-north1-docker.pkg.dev/winged-plate-378113/chonky-repository:${GIT_COMMIT} .
+docker push europe-north1-docker.pkg.dev/winged-plate-378113/chonky-repository:${GIT_COMMIT}
